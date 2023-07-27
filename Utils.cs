fbcd448e59ba774324bdf36d3dc52a055c83be4a
Yona Bystedt <yona.bee@outlook.com>
2023-07-26 18:11:07 -0700

    added some crayons

--- Utils.cs
+++ Utils.cs
@@ -5,6 +5,61 @@
 
         public enum Face { All, Top, Bottom, Left, Right, Front, Back }
 
+        public static string[] Crayons = new[] {
+            "#fc6fcf",
+            "#fc66ff",
+            "#c6f",
+            "#66f",
+            "#6cf",
+            "#6ff",
+            "#6fc",
+            "#6f6",
+            "#cf6",
+            "#ff6",
+            "#fecc66",
+            "#fc6666",
+
+            "#fb0207",
+            "#fd8008",
+            "#ffff0a",
+            "#80ff08",
+            "#21ff06",
+            "#21ff80",
+            "#21ffff",
+            "#0f80ff",
+            "#00f",
+            "#8000ff",
+            "#fb02ff",
+            "#fb0280",
+
+            "#800040",
+            "#800080",
+            "#400080",
+            "#000080",
+            "#074080",
+            "#108080",
+            "#118040",
+            "#118002",
+            "#408002",
+            "#808004",
+            "#804003",
+            "#804003",
+            "#800002",
+
+            "#000",
+            "#191919",
+            "#333",
+            "#4c4c4c",
+            "#666",
+            "#7f7f7f",
+            "#808080",
+            "#999",
+            "#b3b3b3",
+            "#ccc",
+            "#e6e6e6",
+            "#fff"
+        };
+
         public static Vector3 CubeToSphere(Vector3 point)
         {
             return point.Normalized();
