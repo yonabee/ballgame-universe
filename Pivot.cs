f428e178f66df9f3cbd12a52691397f5e60b66ee
Yona Bystedt <yona.bee@outlook.com>
2023-08-06 17:58:36 -0700

    mouselook

--- Pivot.cs
+++ Pivot.cs
@@ -8,16 +8,12 @@ public partial class Pivot : Marker3D
 	public bool OrientForward = false;
 	public Camera3D Camera;
 
+	private Vector2 _cameraRotation;
+
 	public override void _PhysicsProcess(double delta)
 	{
 		if (Universe.Planet.IsQueuedForDeletion()) {
 			return;
-		}
-		if (Input.IsActionPressed("camera_right")) {
-			RotateObjectLocal(OrientForward ? Vector3.Down : Vector3.Up, (float)delta * (OrientForward ? 1f : Speed));
-		}
-		if (Input.IsActionPressed("camera_left")) {
-			RotateObjectLocal(OrientForward ? Vector3.Up : Vector3.Down, (float)delta * (OrientForward ? 1f : Speed));
 		}
 		if (Input.IsActionPressed("camera_up")) {
 			RotateObjectLocal(Vector3.Left, (float)delta * Speed);
@@ -25,17 +21,36 @@ public partial class Pivot : Marker3D
 		if (Input.IsActionPressed("camera_down")) {
 			RotateObjectLocal(Vector3.Right, (float)delta * Speed);
 		}
-		if (Input.IsActionPressed("camera_strafe_left")) {
-			RotateObjectLocal(Vector3.Back, (float)delta * Speed);
+		if (Input.IsActionPressed("camera_left")) {
+			if (Universe.PlayerCam.Current) {
+				RotateObjectLocal(Vector3.Back, (float)delta * Speed);
+			} else {
+				RotateObjectLocal(OrientForward ? Vector3.Up : Vector3.Down, (float)delta * (OrientForward ? 1f : Speed));
+			}
 		}
-		if (Input.IsActionPressed("camera_strafe_right")) {
-			RotateObjectLocal(Vector3.Forward, (float)delta * Speed);
+		if (Input.IsActionPressed("camera_right")) {
+			if (Universe.PlayerCam.Current) {
+				RotateObjectLocal(Vector3.Forward, (float)delta * Speed);
+			} else {
+				RotateObjectLocal(OrientForward ? Vector3.Down : Vector3.Up, (float)delta * (OrientForward ? 1f : Speed));
+			}
 		}
+		RotateObjectLocal(Vector3.Down, _cameraRotation.X * (float)delta * Speed * 2);
+
 		if (Camera == null) {
 			return;
 		}
 
-		Vector3 cubemap = Utils.SphereToCube(Universe.Planet.ToLocal(Camera.ToGlobal(Camera.Transform.Origin)));
+		Camera.RotateObjectLocal(Vector3.Left, _cameraRotation.Y * (float)delta * Speed * 2);
+
+		Vector3 cubemap = Utils.SphereToCube(
+			Universe.Planet.ToLocal(
+				Universe.PlayerPivot.ToGlobal(
+					Universe.PlayerPivot.Transform.Origin 
+						+ new Vector3(0f,Camera.Transform.Origin.Y, 0f)
+				)
+			)
+		);
 		Face face = Utils.GetFace(cubemap);
 		int x = 0;
 		int y = 0;
@@ -43,48 +58,54 @@ public partial class Pivot : Marker3D
 			case Face.Top:
 				x = Mathf.FloorToInt((cubemap.X + 1f) / 2f * Universe.Planet.Resolution); 
 				y = Mathf.FloorToInt((1f - ((cubemap.Z + 1f) / 2f)) * Universe.Planet.Resolution);
-				// GD.Print("Top");
 				break;
 			case Face.Bottom:
 				x = Mathf.FloorToInt((1f - ((cubemap.X + 1f) / 2f)) * Universe.Planet.Resolution); 
 				y = Mathf.FloorToInt((1f - ((cubemap.Z + 1f) / 2f)) * Universe.Planet.Resolution);
-				// GD.Print("Bottom");
 				break;
 			case Face.Left:
 				x = Mathf.FloorToInt((1f - ((cubemap.Z + 1f) / 2f)) * Universe.Planet.Resolution); 
 				y = Mathf.FloorToInt((1f - ((cubemap.Y + 1f) / 2f)) * Universe.Planet.Resolution);
-				// GD.Print("Left");
 				break;
 			case Face.Right:
 				x = Mathf.FloorToInt((cubemap.Z + 1f) / 2f * Universe.Planet.Resolution); 
 				y = Mathf.FloorToInt((1f - ((cubemap.Y + 1f) / 2f)) * Universe.Planet.Resolution);
-				// GD.Print("Right");
 				break;
 			case Face.Front:
 				x = Mathf.FloorToInt((1f- ((cubemap.Y + 1f) / 2f)) * Universe.Planet.Resolution); 
 				y = Mathf.FloorToInt((1f - ((cubemap.X + 1f) / 2f)) * Universe.Planet.Resolution);
-				// GD.Print("Front");
 				break;
 			case Face.Back:
 				x = Mathf.FloorToInt((cubemap.Y + 1f) / 2f * Universe.Planet.Resolution); 
 				y = Mathf.FloorToInt((1f - ((cubemap.X + 1f) / 2f)) * Universe.Planet.Resolution);
-				// GD.Print("Back");
 				break;
 		}
 		Universe.CurrentFace = face;
 		Universe.Location = new Vector2(x,y);
+
 		GD.Print(Universe.CurrentFace + ": " + Universe.Location);
+
 		TerrainFace terrainFace = Universe.Planet.TerrainFaces[(int)face - 1];
 		if (x >= terrainFace.Elevations.GetLength(0) || x < 0 || y >= terrainFace.Elevations.GetLength(1) || y < 0) {
 			return;
 		}
 		float elevation = terrainFace.Elevations[x,y].scaled;
-		// GD.Print(elevation);
 		float offset = (
 				Camera.Transform.Origin.Y - 
 				(Mathf.Max(elevation, Universe.Planet.Radius) + 50f)
 			) * (float)delta * 10;
-		//GD.Print(offset);
+
 		Camera.TranslateObjectLocal(new Vector3(0, -offset, 0));
+
+		_cameraRotation = Vector2.Zero;
 	}
+
+    public override void _Input(InputEvent @event)
+    {
+        if(@event is InputEventMouseMotion mouseMotion && Camera != null)
+		{
+			_cameraRotation.X = mouseMotion.Relative.X;
+			_cameraRotation.Y = mouseMotion.Relative.Y;
+		}
+    }
 }
